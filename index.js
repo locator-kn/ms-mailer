fcecc95b1d2db2bb6b8022c52d378a3c93ef5b4d
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-02-27 12:37:35 +0100

    refactor index

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+'use strict';
 
+const path = require('path');
+const pwd = path.join(__dirname, '..', '/.env');
+require('dotenv').config({path: pwd});
 
 const seneca = require('seneca')();
 const mailer = require('./lib/mailer');
+const util = require('ms-utilities');
 const database = require('./lib/database');
 
-require('dotenv').config({path: '../.env'});
 
 // select desired transport method
-const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
+//const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
 const patternPin = 'role:mailer';
 
 // init seneca and expose functions
 
 
 database.connect()
-    .then((db) => {
+    .then(() => {
         seneca
-            .use(transportMethod + '-transport')
-            .add(patternPin + ',cmd:login', mailer.doSomething)
-            .add(patternPin + ',cmd:else', mailer.doSomethingElse)
-            .listen({type: transportMethod, pin: patternPin});
+        //.use(transportMethod + '-transport')
+
+            .client({type: 'tcp', port: 7010, host: 'localhost', pin: 'role:reporter'})
+
+            .add(patternPin + ',cmd:register,entity:device', mailer.sendMail)
+
+
+            .listen({type: 'tcp', port: 7002, pin: patternPin})
+            .wrap(patternPin, util.reporter.report);
     });
